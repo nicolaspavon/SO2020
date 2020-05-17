16591c08cc6bb5aa5c4bb24df53db3004188d08b
mgonnet <gonnet.matias@gmail.com>
2020-05-16 21:31:07 -0300

    leer datos

--- ProyectoSO/src/proyectoso/ProyectoSO.java
+++ ProyectoSO/src/proyectoso/ProyectoSO.java
@@ -5,6 +5,11 @@
  */
 package proyectoso;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import javafx.util.Pair;
+
 /**
  *
  * @author nico
@@ -15,6 +20,8 @@ public class ProyectoSO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        List<Pair<Integer, Vehiculo>> lista = leer();
+        
         Reloj reloj = new Reloj(5, 3);
         Casilla casilla0 = new Casilla(0, reloj);
         Casilla casilla1 = new Casilla(1, reloj);
@@ -26,4 +33,21 @@ public class ProyectoSO {
         casilla2.start();
     }
     
+    static public List<Pair<Integer, Vehiculo>> leer(){
+        Scanner in = new Scanner(System.in);
+        List<Pair<Integer, Vehiculo>> list = new ArrayList<>();
+        boolean endInput = false;
+        do{
+          String line = in.nextLine();
+          line = line.trim();
+          if(line.equals("*")){
+            endInput = true;
+          }else{
+            String[] datos = line.split("-");
+            list.add(
+                new Pair<>(Integer.parseInt(datos[0]), Vehiculo.parsear(datos[1])));
+          }           
+        }while(!endInput);
+    return list;
+  }
 }
--- ProyectoSO/src/proyectoso/Vehiculo.java
+++ ProyectoSO/src/proyectoso/Vehiculo.java
@@ -9,6 +9,30 @@ package proyectoso;
  *
  * @author nico
  */
+
 public class Vehiculo {
+    public enum TipoVehiculo { 
+        Auto, Omnibus 
+    };
     
+    private String matricula;
+    private TipoVehiculo tipo;
+    
+    public Vehiculo (String matricula, TipoVehiculo tipo) {
+        this.matricula = matricula;
+        this.tipo = tipo;
+    }
+    
+    public static Vehiculo parsear(String raw) {
+        String[] datos = raw.split(",");
+        TipoVehiculo tipo;
+        switch(datos[1]) {
+            case "auto": tipo = TipoVehiculo.Auto; break;
+            case "omnibus": tipo = TipoVehiculo.Omnibus; break;
+            default: throw new Error("Tipo vehiculo desconocido " + raw);
+        }
+                
+        Vehiculo v = new Vehiculo(datos[0], tipo);
+        return v;
+    }
 }
